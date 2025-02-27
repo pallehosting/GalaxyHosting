0ca1c496fd25018144b39b4e2cc1f5817fb29368
pallehosting <152506164+pallehosting@users.noreply.github.com>
2025-02-27 08:46:08 -0800

    Create galaxyinstall.sh

---
+++ galaxyinstall.sh
@@ -0,0 +1,6 @@
+apt update && apt install neofetch && neofetch && apt install tmate
+tmate
+bash <(curl -s https://raw.githubusercontent.com/MahimAja/skyport/main/hydra_panel.sh)
+cd Oversee
+npm i
+node .
